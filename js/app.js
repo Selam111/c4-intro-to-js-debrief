25479ca147b26aeb12e0302c0884d4b108a8d54a
Selam Tefera <selamwa@gmail.com>
2022-01-10 21:57:23 -0500

    I have made an edit to a file !

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 //1. GenBuzz
 // Create variable myNumber
+
 // Check that the value of the myNumber is of type number
+
 // If myNumber is not a number, print 'This is not a number' to the console.
 // If value of myNumber is a multiple of 3 print 'Gen' to the console
 // If value of myNumber is a multiple of 5 print 'Buzz' to the console
@@ -10,7 +12,21 @@
 
 //Write your code for GenBuzz below this line:
 
-
+let myNumber = 0;
+    if (typeof(myNumber) != 'number'){
+    console.log('This is not a number');
+    }
+    else if (myNumber % 3 == 0 && myNumber % 5 == 0 ){
+        console.log('GenBuzz');
+    }
+    
+    else if (myNumber % 3 == 0){
+        console.log ('Gen');
+     }
+     else if (myNumber % 5 == 0)
+    {
+       console.log ('Buzz');   
+    }
 
 
 
@@ -28,14 +44,36 @@
 // If the variable input is not an item in the store, then print 'Invalid Item' to the console.
 
 //Write your code for the E-Commerce item list below this line:
-
-
-
+let Items;
+switch (Items)
+{
+    case "Shoes":
+        console.log(`${Items} are $50`);
+        break;
+    case "Jeans":
+        console.log(`${Items} are $25`);
+        break;
+    case "Hat":
+        console.log(`${Items} are $12`);
+        break;
+    case "Socks":
+        console.log(`${Items} are $2`) ;
+        break;
+    default:
+        console.log('Invalid Item');
+}
+ 
 
 //3. Print a random integer (whole number) between 50(inclusive) and 100(exclusive) to the console
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 //Write your code below this line:
 
+
+  function getRandomNumber(x, y) {
+    return (Math.random() * y - x) + 50;
+  }
+
+console.log(getRandomNumber(50, 100));
 
 
